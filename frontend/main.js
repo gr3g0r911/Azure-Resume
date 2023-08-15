2ff89d4577cafa380ae8e1ffa0b4a754d077fdc0
Grzegorz <grz3siek@gmail.com>
2023-08-16 00:41:51 +0200

    minor changes

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterdotnet.azurewebsites.net/api/GetResumeCounter?code=NvFmy_2Ej2RWo3eh1CzswOPE4lhRAtkdfGG9MCS9EX6bAzFu46ptsg=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
